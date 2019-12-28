897366265155b490922d8156ca5ba3c1915a0df4
Remon <mr248903@gmail.com>
2019-12-28 20:34:34 +0600

    Update

--- Tempo/Model/weatherManager.swift
+++ Tempo/Model/weatherManager.swift
@@ -9,12 +9,42 @@
 import Foundation
 
 struct weatherManager {
-    let URL = "http://api.openweathermap.org/data/2.5/weather?appid=7013acc9bca4f12a02f7fe47b7646e5c&units=metric"
+    let adress = "https://api.openweathermap.org/data/2.5/weather?appid=7013acc9bca4f12a02f7fe47b7646e5c&units=metric"
     
     
     func fetchWeather(cityName: String){
-        let URLstring = URL + "&q=" + cityName
+        let URLstring = adress + "&q=" + cityName
         
-        print(URLstring)
+        request(urlstring: URLstring)
+    }
+    
+    func request(urlstring: String){
+            //create URl
+        if let url = URL(string: urlstring){
+            
+            //create url session
+            let session = URLSession(configuration: .default)
+            
+            //create url session task
+            let task = session.dataTask(with: url, completionHandler: handle(data: response:  error: ) )
+            
+            task.resume()
+            
+            
+        }
+    }
+    
+    func handle(data: Data?, response: URLResponse?, error: Error?){
+        
+        if error != nil{
+            return
+        }
+        
+        else if let safeData = data{
+            let dataString = String(data: safeData, encoding: .utf8)
+                
+            print(dataString!)
+        }
+        
     }
 }
