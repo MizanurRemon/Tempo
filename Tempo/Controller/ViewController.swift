779f546b219ed315aac5372c64b367117cc445fc
remon <remonmizanur@gmail.com>
2023-07-13 17:46:06 +0600

    table

---
+++ Tempo/Controller/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  Tempo
+//
+//  Created by Mizanur Remon on 13/7/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+    }
+
+
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    
+}
+
